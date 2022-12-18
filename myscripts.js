1c30ce0f11d6588d4e2d58e072bc147c5a13d5a5
devil-ahdjoz <120767609+devil-ahdjoz@users.noreply.github.com>
2022-12-18 13:22:10 +0800

    Add files via upload

---
+++ myscripts.js
@@ -0,0 +1,15 @@
+function myFunction() {
+    alert("Hello! Happy monthsary hehehe. I just want to say thank you so much sa lahat ng efforts u did to me and to us. I appreciate it a lot more than you know. To be honest medyo nahihiya na rin ako dahil ang dami mo nang ginawa for me and feeling ko hindi enough yung ginagawa ko to return it to you pero wala akong magawa kundi ang ihatid ka pauwi at pumunta malapit sa inyo na hindi naman lagi compared sa pag punta mo here just to be with me.Sobrang thankful ako na nagkaron ako ng partner na katulad mo. I won't ask for anyone else dahil sobrang swerte ko na sayo. I want to spend the rest of my life with you till we're grey and old. I won't get tired of making you feel that I love you so much kasi mahal naman talaga kita and gusto ko lagi kang at peace. If ever I want to reduce your overthinking so advance sorry na agad if you I am annoying na on your end. Mahal na mahal kita James Clarence Albano, araw-araw kitang pipiliing mahalin at intindihin. Cheers to our 2 months of bangayan ^^ nasa away era na tayo ang moody mo kasi hays pakibawasan naman pls hehehe. Bilis ng panahon no? Parang kelan lang sabi mo you're not looking for someone tas ngayon 2 months na tayo BWHAHAHAHAHHA buti pinatulan mo ko (amen). I love you so much bb. Araw-araw kitang nami-miss. Mag tanan na tayo para hapi tayo both :>>");
+}
+
+var fullImgBox = document.getElementById("fullImgBox");
+var fullImg = document.getElementById("fullImg");
+
+function openFullImg(pic){
+    fullImgBox.style.display = "flex";
+    fullImg.src = pic;
+}
+function closeFullImg(){
+    fullImgBox.style.display = "none";
+}
+
