d655fb74733abdde76e985153151d27aa512d2c8
devil-ahdjoz <120767609+devil-ahdjoz@users.noreply.github.com>
2022-12-31 23:30:28 +0800

    Add files via upload

--- myscripts.js
+++ myscripts.js
@@ -1,5 +1,5 @@
 function myFunction() {
-    alert("Hello! Happy monthsary hehehe. Honestly, I didn't expect na we will end up together kasi nga u're not looking for someone nga diba? (hays sakit mo na). I just want to say thank you so much sa lahat ng efforts na u did for me and for us. Thank you sa paglu-luto at pagda-dala ng foods, sa pangli-libre whenever wala na kong allowance, sa pag-intindi sakin pag may nararamdaman ako, sa pag-sama sakin, sa pag-support sa lahat ng gusto ko, at higit sa lahat, sa pagma-mahal sakin :>. I appreciate it a lot more than you know. To be honest medyo nahihiya na rin ako dahil ang dami mo nang ginawa for me and feeling ko hindi enough yung ginagawa ko to return it to you pero wala akong magawa kundi ang ihatid ka pauwi at pumunta malapit sa inyo na hindi naman lagi compared sa pag punta mo here just to be with me.Sobrang thankful ako na nagkaron ako ng partner na katulad mo. I won't ask for anyone else dahil sobrang swerte ko na sayo. I want to spend the rest of my life with you till we're grey and old. I won't get tired of making you feel that I love you so much kasi mahal naman talaga kita and gusto ko lagi kang at peace. If ever I want to reduce your overthinking so advance sorry na agad if you I am annoying na on your end. Mahal na mahal kita James Clarence Albano, tulad nga nung sa kanta nung concert, araw-araw kitang pipiliin. Araw-araw kitang pipiliing mahalin at intindihin. Cheers to our 2 months of bangayan ^^ nasa away era na tayo ang moody mo kasi hays pakibawasan naman pls hehehe. Bilis ng panahon no? Parang kelan lang sabi mo you're not looking for someone tas ngayon 2 months na tayo BWHAHAHAHAHHA buti pinatulan mo ko (amen). I love you so much bb. Araw-araw kitang nami-miss. Mag tanan na tayo para hapi tayo both :>>");
+    alert("Hello! I just want to let u know na I'm so lucky to have u. U made my yr so special even tho 3 months palang tayo nagkakilala. U brought colors to my dull life. U make me happy everyday. U make me at peace lalo na when we're together. I want to spend my life with you and I know na you'll make it happen, so thank you. I appreciate everything you did to me. Sobrang bait mo, u deserve all the things u got this year (kasama ako sympre hehe). I know na I have many shortcomings sayo and sa ating relationship and I'm sorry if nahu-hurt kita dahil don. Please tell me anything that hurt u. I'll be better the next days ahead. Pls know na u and ur thoughts are valid even if u think na small lang, share it with me ha. Would love to hear your thoughts so that I can adjust. I love you so much, bb. You don't know how thankful I am to have u. Promise me na u won't leave me kasi di kita papayagan hehe. U deserve all the happiness and love that u received. Know that I will always love you no matter what happen (bukod sa cheating). Happy new year! You are my greatest gift of my 2022. Hoping na u'll always be for the next years. I love you so much bading hihi <3");
 }
 
 var fullImgBox = document.getElementById("fullImgBox");
@@ -13,3 +13,16 @@ function closeFullImg(){
     fullImgBox.style.display = "none";
 }
 
+var video = document.querySelectorAll('video')
+
+video.forEach(play => play.addEventListener('click', ()=>{
+
+    play.classList.toggle('active')
+
+    if(play.paused){
+        play.play();
+    }else{
+        play.pause();
+        play.currentTime = 0;
+    }
+}));
